8097751e259a27835387ee92d9d1b4c3b8301f80
Wiktoria Mikołajów <mikolajow.w@gmail.com>
2018-03-23 09:36:31 +0100

    finished task

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '316488014091-1ae84u0kgou1c3616vpk0ohpn3ag6p4o.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'M9Gb_yGK4dQvpqwGss2HUuan',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
